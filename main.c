cea92cf17084117e3109df58fad947919a35e384
calvares <calvares@student.42lisboa.com>
2025-12-11 14:17:17 +0000

    the first files: main for the main function, push_swap for the algorythm, .h for the structs and functions declaration and Makefile to compile everything

---
+++ main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calvares <calvares@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 13:57:27 by calvares          #+#    #+#             */
+/*   Updated: 2025/12/11 14:01:57 by calvares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int ac, char **av)
+{
+	//declarar e inicializar as stacks(pointers)
+	//verificar os argumentos
+	//funcao que cria a stack a de acordo com os argumentos
+	//se a stack nao estiver em ordem:
+		//algoritmos
+}
---
+++ push_swap.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calvares <calvares@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 13:55:05 by calvares          #+#    #+#             */
+/*   Updated: 2025/12/11 13:56:32 by calvares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+//Definir a estrutura caracteristica das stacks a e b
+
+#endif
